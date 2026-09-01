e0892bd370775e75572924e9c507c9fdea7cf452
Shuddhi <kodillayshuddhi@gmail.com>
2026-09-01 18:42:10 +0530

    Create and store expense in vector

--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
+// holds each expense values
+struct Expense {
+    double amt;
+    string category;
+    string desc;
+};
+
+//adding a function to create expense and add it to a vector
+void addExpense(vector<Expense>& expenses){
+    Expense exp;
+
+    cout << "Enter amount:" <<endl;
+    cin >> exp.amt;
+    cout << "Enter category:" <<endl;
+    cin >> exp.category;
+    cout << "Enter small description:" <<endl;
+    cin >> exp.desc;
+
+    expenses.push_back(exp);
+
+}
+
 int main() {
-    cout << "Expense Tracker";
-    
+    cout << "Expense Tracker" <<endl;
+
+
     return 0;
 }
